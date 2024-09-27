5c616b775b1ccb5909030e2c2d48985d96ee8f2f
Lucio-077 <lucioborelli94@gmail.com>
2024-09-27 14:34:29 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    char nome,sesso;
+    printf("Digitare il nome: ");
+    scanf("%c",&nome);
+    getchar();
+    printf("sesso (Maschio o Femmina) : ");
+    scanf("%c",&sesso);
+    if(sesso == 'M') {
+        printf("Egreggio signor %c\n", nome)
+        else
+            printf("Gentilissima signora %c\n", nome);
+}
