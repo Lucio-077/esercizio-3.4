981b08e22d1b1127d439bcf2254fe633c334d942
Lucio-077 <lucioborelli94@gmail.com>
2024-09-27 14:40:05 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,13 +2,12 @@
 
 int main(void) {
     char nome,sesso;
-    printf("Digitare il nome: ");
+    printf("Digitare il nome(solo iniziale): ");
     scanf("%c",&nome);
     getchar();
     printf("sesso (Maschio o Femmina) : ");
     scanf("%c",&sesso);
     if(sesso == 'M') {
-        printf("Egreggio signor %c\n", nome)
-        else
-            printf("Gentilissima signora %c\n", nome);
+        printf("Egreggio signor %c\n", nome);
+    } else {printf("Gentilissima signora %c\n", nome);}
 }
